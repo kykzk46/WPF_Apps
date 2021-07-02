34140dc9bde399c283b430f205b1aae9280724f1
kykzk46 <mwckkwork@gmail.com>
2021-07-02 18:06:03 +0800

    Add hierarchical Listboxes project

---
+++ HierarchicalListboxes/HierarchicalListboxes/viewmodel/ListboxItem_VM.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmFoundation.Wpf;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace HierarchicalListboxes.viewmodel
+{
+	class ListboxItem_VM : ObservableObject
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="items"></param>
+		/// <param name="hierarchyLevel">Determine the useful character of ListBoxSelectedItem</param>
+		/// <param name="childListbox"></param>
+		public ListboxItem_VM(List<string> items, int hierarchyLevel)
+		{
+			_allItems = items;
+			_ListBoxVisibleItems = new ObservableCollection<string>(_allItems);
+			_hierarchyLevel = hierarchyLevel;
+		}
+
+		int _hierarchyLevel;
+		List<string> _allItems;
+		public ListboxItem_VM ChildListbox;
+
+		ObservableCollection<string> _ListBoxVisibleItems;
+        public ObservableCollection<string> ListBoxVisibleItems 
+        {
+            get { return _ListBoxVisibleItems; }
+            set
+            {
+                if (value != _ListBoxVisibleItems)
+                {
+                    _ListBoxVisibleItems = value;
+                    RaisePropertyChanged("ListBoxVisibleItems");
+
+					if (ChildListbox != null)
+						ChildListbox.SetFilter4ItemSources(_ListBoxSelectedItem.Substring(0, _hierarchyLevel));
+                }
+            }
+        }
+
+		string _ListBoxSelectedItem = "";
+        public string ListBoxSelectedItem 
+        {
+            get { return _ListBoxSelectedItem; }
+            set
+            {
+                if (value != _ListBoxSelectedItem)
+                {
+                    _ListBoxSelectedItem = value;
+                    RaisePropertyChanged("ListBoxSelectedItem");
+
+					if (ChildListbox != null)
+						ChildListbox.SetFilter4ItemSources(_ListBoxSelectedItem.Substring(0, _hierarchyLevel));
+                }
+            }
+        }
+
+		RelayCommand<object> _ListBoxSelectionChanged = null;
+        public ICommand ListBoxSelectionChanged
+        {
+            get
+            {
+                if (_ListBoxSelectionChanged == null)
+                {
+                    _ListBoxSelectionChanged = new RelayCommand<object>(p => OnListBoxSelectionChanged(p), (p) => CanListBoxSelectionChanged(p));
+                }
+
+                return _ListBoxSelectionChanged;
+            }
+        }
+        bool CanListBoxSelectionChanged(object param)
+        {
+            return true;
+        }
+
+        private void OnListBoxSelectionChanged(object param)
+        {
+
+        }
+
+		public void SetFilter4ItemSources(string filter)
+		{
+			ListBoxVisibleItems = new ObservableCollection<string>(
+				from i in _allItems
+				where i.StartsWith(filter)
+				select i);
+		}
+	}
+}
---
+++ HierarchicalListboxes/HierarchicalListboxes/viewmodel/MainWindow_VM.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using MvvmFoundation.Wpf;
+using System.Windows.Input;
+
+namespace HierarchicalListboxes.viewmodel
+{
+	class MainWindow_VM : ObservableObject
+	{
+		List<string> _listbox1Items = new List<string>
+		{
+			"A000",
+			"A000",
+			"A000",
+			"A000",
+			"A000",
+			"A000",
+			"B000",
+			"B000",
+			"B000",
+			"B000",
+			"C000",
+			"C000",
+			"C000",
+			"C000",
+		};
+
+		List<string> _listbox2Items = new List<string>
+		{
+			"A100",
+			"A100",
+			"A200",
+			"A200",
+			"A300",
+			"A300",
+			"B100",
+			"B100",
+			"B200",
+			"B200",
+			"C100",
+			"C100",
+			"C100",
+			"C200",
+		};
+
+		List<string> _listbox3Items = new List<string>
+		{
+			"A110",
+			"A110",
+			"A210",
+			"A210",
+			"A310",
+			"A320",
+			"B110",
+			"B110",
+			"B210",
+			"B220",
+			"C110",
+			"C110",
+			"C120",
+			"C210",
+		};
+
+		List<string> _listbox4Items = new List<string>
+		{
+			"A111",
+			"A112",
+			"A211",
+			"A212",
+			"A311",
+			"A321",
+			"B111",
+			"B112",
+			"B211",
+			"B222",
+			"C111",
+			"C112",
+			"C121",
+			"C211",
+		};
+
+		ListboxItem_VM _Listbox1Vm;
+		public ListboxItem_VM Listbox1Vm
+		{
+			get { return _Listbox1Vm; }
+			set
+			{
+				if(_Listbox1Vm != value)
+				{
+					_Listbox1Vm = value;
+					RaisePropertyChanged("Listbox1Vm");
+				}
+			}
+		}
+
+		ListboxItem_VM _Listbox2Vm;
+		public ListboxItem_VM Listbox2Vm
+		{
+			get { return _Listbox2Vm; }
+			set
+			{
+				if(_Listbox2Vm != value)
+				{
+					_Listbox2Vm = value;
+					RaisePropertyChanged("Listbox2Vm");
+				}
+			}
+		}
+
+		ListboxItem_VM _Listbox3Vm;
+		public ListboxItem_VM Listbox3Vm
+		{
+			get { return _Listbox3Vm; }
+			set
+			{
+				if(_Listbox3Vm != value)
+				{
+					_Listbox3Vm = value;
+					RaisePropertyChanged("Listbox3Vm");
+				}
+			}
+		}
+
+		ListboxItem_VM _Listbox4Vm;
+		public ListboxItem_VM Listbox4Vm
+		{
+			get { return _Listbox4Vm; }
+			set
+			{
+				if(_Listbox4Vm != value)
+				{
+					_Listbox4Vm = value;
+					RaisePropertyChanged("Listbox4Vm");
+				}
+			}
+		}
+
+		public MainWindow_VM()
+		{
+			_Listbox1Vm = new ListboxItem_VM(_listbox1Items.Distinct<string>().ToList(), 1);
+			_Listbox2Vm = new ListboxItem_VM(_listbox2Items.Distinct<string>().ToList(), 2);
+			_Listbox3Vm = new ListboxItem_VM(_listbox3Items.Distinct<string>().ToList(), 3);
+			_Listbox4Vm = new ListboxItem_VM(_listbox4Items.Distinct<string>().ToList(), 4);
+
+			_Listbox1Vm.ChildListbox = _Listbox2Vm;
+			_Listbox2Vm.ChildListbox = _Listbox3Vm;
+			_Listbox3Vm.ChildListbox = _Listbox4Vm;
+		}
+
+	}
+}
